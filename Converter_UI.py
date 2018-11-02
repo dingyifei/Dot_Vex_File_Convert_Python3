3ffd533c936924d5bc55e573dc2db24312d58f7b
Yifei Ding <dingyifeiair@gmail.com>
2018-11-02 13:06:15 -0700

    i hate tabs

--- Converter_UI.py
+++ Converter_UI.py
@@ -10,217 +10,214 @@ from tkinter import messagebox
 
 
 def main():
-	root = Tk()
-	root.title(".vex Converter")
-	mainframe = Frame(root)
-	mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-	root.columnconfigure(0, weight=1)
-	root.rowconfigure(0, weight=1)
+    root = Tk()
+    root.title(".vex Converter")
+    mainframe = Frame(root)
+    mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
 
-	# variables (Changeable)
-	code_folder = StringVar()
-	temp_folder = StringVar()
-	vex_open = StringVar()
-	vex_save_folder = StringVar()
-	vex_save_name = StringVar()
-	replace = BooleanVar()
+    # variables (Changeable)
+    code_folder = StringVar()
+    temp_folder = StringVar()
+    vex_open = StringVar()
+    vex_save_folder = StringVar()
+    vex_save_name = StringVar()
+    replace = BooleanVar()
 
-	# Column 1
-	status_label = Label(mainframe, text="Status: ")
-	status_label.grid(column=1, row=1, sticky=(W, N))
+    # Column 1
+    status_label = Label(mainframe, text="Status: ")
+    status_label.grid(column=1, row=1, sticky=(W, N))
 
-	code_folder_label = Label(mainframe, text="Code Folder: ")
-	code_folder_label.grid(column=1, row=2, sticky=(W, N))
+    code_folder_label = Label(mainframe, text="Code Folder: ")
+    code_folder_label.grid(column=1, row=2, sticky=(W, N))
 
-	temp_folder_label = Label(mainframe, text="Temp Folder: ")
-	temp_folder_label.grid(column=1, row=3, sticky=(W, N))
+    temp_folder_label = Label(mainframe, text="Temp Folder: ")
+    temp_folder_label.grid(column=1, row=3, sticky=(W, N))
 
-	vex_open_label = Label(mainframe, text=".vex Open: ")
-	vex_open_label.grid(column=1, row=4, sticky=(W, N))
+    vex_open_label = Label(mainframe, text=".vex Open: ")
+    vex_open_label.grid(column=1, row=4, sticky=(W, N))
 
-	vex_save_folder_label = Label(mainframe, text=".vex Save Folder: ")
-	vex_save_folder_label.grid(column=1, row=5, sticky=(W, N))
+    vex_save_folder_label = Label(mainframe, text=".vex Save Folder: ")
+    vex_save_folder_label.grid(column=1, row=5, sticky=(W, N))
 
-	vex_save_name_label = Label(mainframe, text=".vex Save Name: ")
-	vex_save_name_label.grid(column=1, row=6, sticky=(W, N))
+    vex_save_name_label = Label(mainframe, text=".vex Save Name: ")
+    vex_save_name_label.grid(column=1, row=6, sticky=(W, N))
 
-	def extract_decode():
-		if temp_folder.get() != "":
-			dot_vex_convert.DEFAULT_TEMP_FILE_LOCATION = temp_folder.get()
-		dot_vex_convert.extract_dot_vex(vex_open.get(), code_folder.get(), progress_show_label.set)
-		progress_show_label["text"] = "extract complete"
+    def extract_decode():
+        if temp_folder.get() != "":
+            dot_vex_convert.DEFAULT_TEMP_FILE_LOCATION = temp_folder.get()
+        dot_vex_convert.extract_dot_vex(vex_open.get(), code_folder.get(), progress_show_label)
+        progress_show_label["text"] = "extract complete"
 
-	extract_decode_button = Button(
-		mainframe, text="Extract and Decode", command=extract_decode)
-	extract_decode_button.grid(column=1, row=7, sticky=(N, W))
+    extract_decode_button = Button(
+        mainframe, text="Extract and Decode", command=extract_decode)
+    extract_decode_button.grid(column=1, row=7, sticky=(N, W))
 
-	convert_type_checkbox = Checkbutton(
-		mainframe, text="Convert between Vex C++ \n and C++ Pro during encode")
-	convert_type_checkbox.grid(column=1, row=8, sticky=(W, N))
+    convert_type_checkbox = Checkbutton(
+        mainframe, text="Convert between Vex C++ \n and C++ Pro during encode")
+    convert_type_checkbox.grid(column=1, row=8, sticky=(W, N))
 
-	progress_text_label = Label(mainframe, text="Progress: ")
-	progress_text_label.grid(column=1, row=9, sticky=(W, N))
+    progress_text_label = Label(mainframe, text="Progress: ")
+    progress_text_label.grid(column=1, row=9, sticky=(W, N))
 
-	# Column 2
-	status_show_label = Label(mainframe, text="Nothing here yet")
-	status_show_label.grid(column=2, row=1, sticky=(N, W))
+    # Column 2
+    status_show_label = Label(mainframe, text="Nothing here yet")
+    status_show_label.grid(column=2, row=1, sticky=(N, W))
 
-	code_folder_entry = Entry(mainframe, width=15, textvariable=code_folder)
-	code_folder_entry.grid(column=2, row=2, sticky=(N, W))
+    code_folder_entry = Entry(mainframe, width=15, textvariable=code_folder)
+    code_folder_entry.grid(column=2, row=2, sticky=(N, W))
 
-	temp_folder_entry = Entry(mainframe, width=15, textvariable=temp_folder)
-	temp_folder_entry.grid(column=2, row=3, sticky=(N, W))
+    temp_folder_entry = Entry(mainframe, width=15, textvariable=temp_folder)
+    temp_folder_entry.grid(column=2, row=3, sticky=(N, W))
 
-	vex_open_entry = Entry(mainframe, width=15, textvariable=vex_open)
-	vex_open_entry.grid(column=2, row=4, sticky=(N, W))
+    vex_open_entry = Entry(mainframe, width=15, textvariable=vex_open)
+    vex_open_entry.grid(column=2, row=4, sticky=(N, W))
 
-	vex_save_folder_entry = Entry(
-		mainframe, width=15, textvariable=vex_save_folder)
-	vex_save_folder_entry.grid(column=2, row=5, sticky=(N, W))
+    vex_save_folder_entry = Entry(
+        mainframe, width=15, textvariable=vex_save_folder)
+    vex_save_folder_entry.grid(column=2, row=5, sticky=(N, W))
 
-	vex_save_name_entry = Entry(mainframe, width=15, textvariable=vex_save_name)
-	vex_save_name_entry.grid(column=2, row=6, sticky=(N, W))
+    vex_save_name_entry = Entry(mainframe, width=15, textvariable=vex_save_name)
+    vex_save_name_entry.grid(column=2, row=6, sticky=(N, W))
 
-	def convert_to_dot_vex():
-		progress_show_label["text"] = "converting"
-		if temp_folder.get != "":
-			dot_vex_convert.DEFAULT_TEMP_FILE_LOCATION = temp_folder.get()
-		dot_vex_convert.update_dot_vex(
-			vex_open.get(),
-			vex_save_folder.get(),
-			vex_save_name.get(),
-			code_folder.get(),
-			progress_show_label.set)
-		progress_show_label["text"] = "convert complete"
-		
-	convert_vex_button = Button(mainframe, text="Convert to .vex File", command=convert_to_dot_vex)
-	convert_vex_button.grid(column=2, row=7, sticky=(N, E))
+    def convert_to_dot_vex():
+        if temp_folder.get != "":
+            dot_vex_convert.DEFAULT_TEMP_FILE_LOCATION = temp_folder.get()
+        dot_vex_convert.update_dot_vex(
+            vex_open.get(),
+            vex_save_folder.get(),
+            vex_save_name.get(),
+            code_folder.get(),
+            progress_show_label["text"])
 
-	def replace_command():
-		if replace.get() == True:
-			head, tail = os.path.split(vex_open.get())
-			vex_save_name.set(tail)
-			vex_save_folder.set(head)
-			vex_save_folder_button["state"] = "disabled"
-			vex_save_folder_entry["state"] = "disabled"
-			vex_save_name_entry["state"] = "disabled"
-		if replace.get() == False:
-			vex_save_folder_button["state"] = "normal"
-			vex_save_folder_entry["state"] = "normal"
-			vex_save_name_entry["state"] = "normal"
+    convert_vex_button = Button(mainframe, text="Convert to .vex File", command=convert_to_dot_vex)
+    convert_vex_button.grid(column=2, row=7, sticky=(N, E))
 
-	replace_checkbox = Checkbutton(mainframe,
-	text="replace old .vex file", command=replace_command, variable=replace)
-	replace_checkbox.grid(column=2, row=8, sticky=(W, N))
+    def replace_command():
+        if replace.get() is True:
+            head, tail = os.path.split(vex_open.get())
+            vex_save_name.set(tail)
+            vex_save_folder.set(head)
+            vex_save_folder_button["state"] = "disabled"
+            vex_save_folder_entry["state"] = "disabled"
+            vex_save_name_entry["state"] = "disabled"
+        if replace.get() is False:
+            vex_save_folder_button["state"] = "normal"
+            vex_save_folder_entry["state"] = "normal"
+            vex_save_name_entry["state"] = "normal"
 
-	progress_show_label = Label(mainframe, text="")
-	progress_show_label.grid(column=2, row=9, sticky=(W, N))
+    replace_checkbox = Checkbutton(mainframe,
+                                   text="replace old .vex file", command=replace_command, variable=replace)
+    replace_checkbox.grid(column=2, row=8, sticky=(W, N))
 
-	# Column 3
+    progress_show_label = Label(mainframe, text="")
+    progress_show_label.grid(column=2, row=9, sticky=(W, N))
 
-	def get_help():
-		help_window = Tk()
-		help_window.title("Help")
-		help_frame = Frame(help_window)
-		help_frame.grid(column=0, row=0, sticky=(N, W, E, S))
-		help_frame.columnconfigure(0, weight=1)
-		help_frame.rowconfigure(0, weight=1)
-		help_label = Label(
-			help_frame,
-			text="this is the test text for helping window \n this is the test text")
-		help_label.grid(column=1, row=1, sticky=(N, E, W, S))
-		help_frame.mainloop()
+    # Column 3
 
-	help_button = Button(mainframe, text="Help", command=get_help)
-	help_button.grid(column=3, row=1, sticky=(N, E))
+    def get_help():
+        help_window = Tk()
+        help_window.title("Help")
+        help_frame = Frame(help_window)
+        help_frame.grid(column=0, row=0, sticky=(N, W, E, S))
+        help_frame.columnconfigure(0, weight=1)
+        help_frame.rowconfigure(0, weight=1)
+        help_label = Label(
+            help_frame,
+            text="this is the test text for helping window \n this is the test text")
+        help_label.grid(column=1, row=1, sticky=(N, E, W, S))
+        help_frame.mainloop()
 
-	def code_folder_ask():
-		code_folder.set(askdirectory())
+    help_button = Button(mainframe, text="Help", command=get_help)
+    help_button.grid(column=3, row=1, sticky=(N, E))
 
-	code_folder_button = Button(mainframe, text="Browse", command=code_folder_ask)
-	code_folder_button.grid(column=3, row=2, sticky=(N, E))
+    def code_folder_ask():
+        code_folder.set(askdirectory())
 
-	def temp_folder_ask():
-		temp_folder.set(askdirectory())
+    code_folder_button = Button(mainframe, text="Browse", command=code_folder_ask)
+    code_folder_button.grid(column=3, row=2, sticky=(N, E))
 
-	temp_folder_button = Button(mainframe, text="Browse", command=temp_folder_ask)
-	temp_folder_button.grid(column=3, row=3, sticky=(N, E))
+    def temp_folder_ask():
+        temp_folder.set(askdirectory())
 
-	def vex_open_ask():
-		vex_open.set(askopenfilename(filetypes=[("Vex files", "*.vex")]))
-		if replace.get() == True:
-			replace_command()
-		else:
-			head, tail = os.path.split(vex_open.get())
-			vex_save_name.set(tail)
+    temp_folder_button = Button(mainframe, text="Browse", command=temp_folder_ask)
+    temp_folder_button.grid(column=3, row=3, sticky=(N, E))
 
-	vex_open_button = Button(mainframe, text="Browse", command=vex_open_ask)
-	vex_open_button.grid(column=3, row=4, sticky=(N, E))
+    def vex_open_ask():
+        vex_open.set(askopenfilename(filetypes=[("Vex files", "*.vex")]))
+        if replace.get() is True:
+            replace_command()
+        else:
+            head, tail = os.path.split(vex_open.get())
+            vex_save_name.set(tail)
 
-	def vex_save_folder_ask():
-		vex_save_folder.set(askdirectory())
+    vex_open_button = Button(mainframe, text="Browse", command=vex_open_ask)
+    vex_open_button.grid(column=3, row=4, sticky=(N, E))
 
-	vex_save_folder_button = Button(
-		mainframe, text="Browse", command=vex_save_folder_ask)
-	vex_save_folder_button.grid(column=3, row=5, sticky=(N, E))
+    def vex_save_folder_ask():
+        vex_save_folder.set(askdirectory())
 
-	# ----------------------------------------------------------------------------------------------------------
-	# load or create config file
-	config = configparser.ConfigParser()
+    vex_save_folder_button = Button(
+        mainframe, text="Browse", command=vex_save_folder_ask)
+    vex_save_folder_button.grid(column=3, row=5, sticky=(N, E))
 
-	def load_config():
-		if os.path.isfile("./vex_convert.ini"):
-			config.read(filenames="./vex_convert.ini")
-			try:
-				code_folder.set(config["DEFAULT"]["code_folder"])
-				temp_folder.set(config["DEFAULT"]["temp_folder"])
-				vex_open.set(config["DEFAULT"]["vex_open"])
-				vex_save_folder.set(config["DEFAULT"]["vex_save_folder"])
-				vex_save_name.set(config["DEFAULT"]["vex_save_name"])
-				replace.set(config["DEFAULT"]["replace"])
-			except:
-				create_config()
-				load_config()
-		else:
-			create_config()
+    # ----------------------------------------------------------------------------------------------------------
+    # load or create config file
+    config = configparser.ConfigParser()
 
-	def create_config():
-		if os.path.isfile("./vex_convert.ini"):
-			os.remove("./vex_convert.ini")
-		config["DEFAULT"] = {
-			"code_folder": "",
-			"temp_folder": "./temp/",
-			"vex_open": "",
-			"vex_save_folder": "",
-			"vex_save_name": "",
-			"replace": "False"
-		}
-		with open("./vex_convert.ini", "w") as configfile:
-			config.write(configfile)
+    def load_config():
+        if os.path.isfile("./vex_convert.ini"):
+            config.read(filenames="./vex_convert.ini")
+            try:
+                code_folder.set(config["DEFAULT"]["code_folder"])
+                temp_folder.set(config["DEFAULT"]["temp_folder"])
+                vex_open.set(config["DEFAULT"]["vex_open"])
+                vex_save_folder.set(config["DEFAULT"]["vex_save_folder"])
+                vex_save_name.set(config["DEFAULT"]["vex_save_name"])
+                replace.set(config["DEFAULT"]["replace"])
+            except:
+                create_config()
+                load_config()
+        else:
+            create_config()
 
-	def save_config():
-		config["DEFAULT"] = {
-			"code_folder": code_folder.get(),
-			"temp_folder": temp_folder.get(),
-			"vex_open": vex_open.get(),
-			"vex_save_folder": vex_save_folder.get(),
-			"vex_save_name": vex_save_name.get(),
-			"replace": replace.get()
-		}
-		with open("./vex_convert.ini", "w") as configfile:
-			config.write(configfile)
+    def create_config():
+        if os.path.isfile("./vex_convert.ini"):
+            os.remove("./vex_convert.ini")
+        config["DEFAULT"] = {
+            "code_folder": "",
+            "temp_folder": "./temp/",
+            "vex_open": "",
+            "vex_save_folder": "",
+            "vex_save_name": "",
+            "replace": "False"
+        }
+        with open("./vex_convert.ini", "w") as configfile:
+            config.write(configfile)
 
-	# When close
-	def window_close():
-		save_config()
-		root.destroy()
+    def save_config():
+        config["DEFAULT"] = {
+            "code_folder": code_folder.get(),
+            "temp_folder": temp_folder.get(),
+            "vex_open": vex_open.get(),
+            "vex_save_folder": vex_save_folder.get(),
+            "vex_save_name": vex_save_name.get(),
+            "replace": replace.get()
+        }
+        with open("./vex_convert.ini", "w") as configfile:
+            config.write(configfile)
 
-	root.protocol("WM_DELETE_WINDOW", window_close)
-	# Start the window
-	load_config()
-	root.mainloop()
+    # When close
+    def window_close():
+        save_config()
+        root.destroy()
+
+    root.protocol("WM_DELETE_WINDOW", window_close)
+    # Start the window
+    load_config()
+    root.mainloop()
 
 
 if __name__ == '__main__':
-	main()
-
+    main()
--- dot_vex_convert.py
+++ dot_vex_convert.py
@@ -1,4 +1,3 @@
-
 # coding=utf-8
 """
 I have nothing to say, it is a very simple program works
@@ -11,100 +10,101 @@ import os
 DEFAULT_TEMP_FILE_LOCATION = "./temp/"
 
 
-def extract_dot_vex(vex_file_location: str,
-					save_folder_location: str,
-					progress=print):
-	"""
+def extract_dot_vex(vex_file_location: str, save_folder_location: str, progress):
+    """
 
     :param vex_file_location: the .vex file location, should include the .vex file
     :param save_folder_location:  the save folder location
     :param progress:  optional way to output the progress
     """
+
     progress("extracting json from .vex tar file")
-	dot_vex_file = tarfile.open(vex_file_location)
-	dot_vex_file.extractall(DEFAULT_TEMP_FILE_LOCATION)
-	dot_vex_file.close()
-	progress("file extracted, loading json)
-	with open(DEFAULT_TEMP_FILE_LOCATION + "___ThIsisATemPoRaRyFiLE___.json") as content:
-		dot_vex_json: dict = json.load(content)
-		if os.path.isdir(save_folder_location)== False:
-			os.mkdir(save_folder_location)
-		progress("extracting and decode files from json")
-		for x in dot_vex_json["files"]:
-			with open(save_folder_location + "/" + x, "wb") as file:
-				file.write(base64.b64decode(dot_vex_json["files"][x]))
-		progress(len(dot_vex_json["files"]) + "Files extracted from .vex")
-		
+    dot_vex_file = tarfile.open(vex_file_location)
+    dot_vex_file.extractall(DEFAULT_TEMP_FILE_LOCATION)
+    dot_vex_file.close()
+    progress("file extracted, loading json")
+    with open(DEFAULT_TEMP_FILE_LOCATION + "___ThIsisATemPoRaRyFiLE___.json") as content:
+        dot_vex_json: dict = json.load(content)
+        if not os.path.isdir(save_folder_location):
+            os.mkdir(save_folder_location)
+        progress("extracting and decode files from json")
+        for x in dot_vex_json["files"]:
+            with open(save_folder_location + "/" + x, "wb") as file:
+                file.write(base64.b64decode(dot_vex_json["files"][x]))
+        progress(len(dot_vex_json["files"]), "Files extracted from .vex")
+
 
 def update_dot_vex(vex_file_location: str,
-					save_folder_location: str, 
-					save_file_name: str,
-					vex_decode_folder_location: str,
-					progress = print
-					):
-	"""
+                   save_folder_location: str,
+                   save_file_name: str,
+                   vex_decode_folder_location: str,
+                   progress=print
+                   ):
+    """
 
     :param vex_file_location:  the old .vex file location, should include the .vex file
     :param save_folder_location: the folder to save .vex file
     :param save_file_name: the name of .vex file. should include .vex
     :param vex_decode_folder_location: the files you want to put into the .vex
-    :param progress:  optional way to output the progress
+    :param progress: optional way to output the progress
     """
-    
-    progress("extracting json from .vex tar file")    
-	dot_vex_file = tarfile.open(vex_file_location)
-	dot_vex_file.extractall(DEFAULT_TEMP_FILE_LOCATION)
-	dot_vex_file.close()
-	if os.path.isdir(save_folder_location)== False:
-		os.mkdir(save_folder_location)
-	progress("loading json")
-	with open(DEFAULT_TEMP_FILE_LOCATION + "___ThIsisATemPoRaRyFiLE___.json") as content:
-		dot_vex_json: dict = json.load(content)
-		encode_files: list = os.listdir(vex_decode_folder_location)
-		progress("replacing file inside json")
-		for x in encode_files:
-			with open(vex_decode_folder_location + x, "rb") as file:
-				dot_vex_json["files"][x] = base64.b64encode(file.read()).decode("utf-8")
-	progress("replace the json file")
-	os.remove(DEFAULT_TEMP_FILE_LOCATION + "___ThIsisATemPoRaRyFiLE___.json")
-	with open(DEFAULT_TEMP_FILE_LOCATION + "___ThIsisATemPoRaRyFiLE___.json", "w") as content:
-		json.dump(dot_vex_json, content)
-	try:
-		os.remove(save_folder_location + "/" + save_file_name)
-	except:
-		progress("we don't need to remove the json file")
-	dot_vex_save_file = tarfile.open(save_folder_location + "/" + save_file_name, "w")
-	dot_vex_save_file.add(
-		DEFAULT_TEMP_FILE_LOCATION +
-		"___ThIsisATemPoRaRyFiLE___.json",
-		"___ThIsisATemPoRaRyFiLE___.json")
 
-	dot_vex_save_file.close()
-	progress("replace/update .vex file complete")
+    progress("extracting json from .vex tar file")
+    dot_vex_file = tarfile.open(vex_file_location)
+    dot_vex_file.extractall(DEFAULT_TEMP_FILE_LOCATION)
+    dot_vex_file.close()
+    if not os.path.isdir(save_folder_location):
+        os.mkdir(save_folder_location)
+    progress("loading json")
+    with open(DEFAULT_TEMP_FILE_LOCATION + "___ThIsisATemPoRaRyFiLE___.json") as content:
+        dot_vex_json: dict = json.load(content)
+        encode_files: list = os.listdir(vex_decode_folder_location)
+        progress("replacing file inside json")
+        for x in encode_files:
+            with open(vex_decode_folder_location + x, "rb") as file:
+                dot_vex_json["files"][x] = base64.b64encode(file.read()).decode("utf-8")
+    progress("replace the json file")
+    os.remove(DEFAULT_TEMP_FILE_LOCATION + "___ThIsisATemPoRaRyFiLE___.json")
+    with open(DEFAULT_TEMP_FILE_LOCATION + "___ThIsisATemPoRaRyFiLE___.json", "w") as content:
+        json.dump(dot_vex_json, content)
+    try:
+        os.remove(save_folder_location + "/" + save_file_name)
+    except:
+        progress("we don't need to remove the json file")
+    dot_vex_save_file = tarfile.open(save_folder_location + "/" + save_file_name, "w")
+    dot_vex_save_file.add(
+        DEFAULT_TEMP_FILE_LOCATION +
+        "___ThIsisATemPoRaRyFiLE___.json",
+        "___ThIsisATemPoRaRyFiLE___.json")
+
+    dot_vex_save_file.close()
+    progress("replace/update .vex file complete")
+
 
 def main():
-	"""
+    """
     I have no idea why someone try to run this and say it is not working....
     """
-    
-    print("You are running it in the console, it have less feature than the converter_ui \n highly suggrest running converter_ui.python instead")
-	
-	mode = input("do you want to extract a .vex file (choose 1), or update a .vex file (Choose 2)?")
-	if mode == 1 then:
-		vex_file = input("vex file location?")
-		save_dir = input("where do you want save the files?")
-		extract_dot_vex(vex_file, save_dir, print)
-		
-	elif mode == 2 then:
-		vex_file = input("vex file location?")
-		save_dir = input("where do you want save the files?")
-		save_name = input("save name?")
-		open_dir = input("What do you want to save into the vex file?")
-		update_dot_vex(vex_file, save_dir, save_name, open)
-		
-	else:
-		print("handle weird input is just annoying")
-		
+
+    print(
+        "You are running it in the console, it have less feature than the converter_ui \n try converter_ui.py")
+
+    mode = input("do you want to extract a .vex file (choose 1), or update a .vex file (Choose 2)?")
+    if mode == 1:
+        vex_file = input("vex file location?")
+        save_dir = input("where do you want save the files?")
+        extract_dot_vex(vex_file, save_dir, print)
+
+    elif mode == 2:
+        vex_file = input("vex file location?")
+        save_dir = input("where do you want save the files?")
+        save_name = input("save name?")
+        open_dir = input("What do you want to save into the vex file?")
+        update_dot_vex(vex_file, save_dir, save_name, open_dir)
+
+    else:
+        print("handle weird input is just annoying")
+
 
 if __name__ == '__main__':
-	main()
+    main()
